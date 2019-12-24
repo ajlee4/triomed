2a6973c44fc55f03101d18fb78aeb7e013b802de
ajlee4 <patsyno94@gmail.com>
2019-12-24 17:04:10 +0300

    add portfolio implantantion-single and specialist-list

--- app/static/js/helpers/slider.js
+++ app/static/js/helpers/slider.js
@@ -1,53 +1,88 @@
-"use strict";
+'use strict';
 
-$(document).ready(function(){
-    $('.slider-specialist__inner').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        infinite: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next')
-    });
+$(document).ready(function() {
+  $('.slider-specialist__inner').slick(
+    {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      infinite: true,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+    },
+    $('.slider-specialist__inner').addClass('created'),
+  );
 
-    $('#sertificat-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        infinite: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next')
-    });
-    $('#specialist-portfolio').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-       
-        infinite: true,
-      
-        prevArrow: $('.prev-arrow'),
-        nextArrow: $('.next-arrow')
-    });
+  $('#sertificat-slider').slick(
+    {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      infinite: true,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+    },
+    $('#sertificat-slider').addClass('created'),
+  );
+  $('#specialist-portfolio').slick(
+    {
+      slidesToShow: 1,
+      slidesToScroll: 1,
 
-    $('#diagnostic-port').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        infinite: true,
-        prevArrow: $('.prev-arrow'),
-        nextArrow: $('.next-arrow')
-    });
+      infinite: true,
 
-    $('#reason-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      
-        prevArrow: $('.prev-arr'),
-        nextArrow: $('.next-arr'),
-     
-    })
-   
-  });
+      prevArrow: $('.prev-arrow'),
+      nextArrow: $('.next-arrow'),
+    },
+    $('#specialist-portfolio').addClass('created'),
+  );
+
+  $('#diagnostic-port').slick(
+    {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      infinite: true,
+      prevArrow: $('.prev-arrow'),
+      nextArrow: $('.next-arrow'),
+    },
+    $('#diagnostic-port').addClass('created'),
+  );
+
+  $('#reason-slider').slick(
+    {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+
+      prevArrow: $('.prev-arr'),
+      nextArrow: $('.next-arr'),
+    },
+    $('#reason-slider').addClass('created'),
+  );
+
+  $('#portfolio-page-slider').slick(
+    {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: $('.portfolio-prev'),
+      nextArrow: $('.portfolio-next'),
+    },
+    $('.portfolio-page-slider').addClass('created'),
+  );
+
+
+  $('#specialist-list-slider').slick(
+    {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: $('.portfolio-prev'),
+      nextArrow: $('.portfolio-next'),
+    },
+    $('.specialist-list-slider').addClass('created'),
+  );
+
+});
